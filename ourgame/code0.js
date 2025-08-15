d4ea4705c941e86864b549eb7c43569663b15acc
Ahmed Sherif <ahmed.sh.hammam@gmail.com>
2025-08-15 20:11:07 +0300

    updates sizes and finals

---
+++ ourgame/code0.js
@@ -0,0 +1,96 @@
+gdjs.Title_32ScreenCode = {};
+gdjs.Title_32ScreenCode.localVariables = [];
+gdjs.Title_32ScreenCode.GDTitlescreenObjects1= [];
+gdjs.Title_32ScreenCode.GDTitlescreenObjects2= [];
+gdjs.Title_32ScreenCode.GDLogoObjects1= [];
+gdjs.Title_32ScreenCode.GDLogoObjects2= [];
+gdjs.Title_32ScreenCode.GDStartGameObjects1= [];
+gdjs.Title_32ScreenCode.GDStartGameObjects2= [];
+
+
+gdjs.Title_32ScreenCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.anyKeyPressed(runtimeScene);
+if (isConditionTrue_0) {
+isConditionTrue_0 = false;
+{isConditionTrue_0 = runtimeScene.getOnceTriggers().triggerOnce(16030772);
+}
+}
+if (isConditionTrue_0) {
+{gdjs.evtTools.sound.playSoundOnChannel(runtimeScene, "Music2.mp3", 1, true, 25, 1);
+}{runtimeScene.getGame().getVariables().getFromIndex(2).setNumber(1);
+}{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "Level 1", false);
+}}
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+if (isConditionTrue_0) {
+gdjs.copyArray(runtimeScene.getObjects("StartGame"), gdjs.Title_32ScreenCode.GDStartGameObjects1);
+{for(var i = 0, len = gdjs.Title_32ScreenCode.GDStartGameObjects1.length ;i < len;++i) {
+    gdjs.Title_32ScreenCode.GDStartGameObjects1[i].getBehavior("Flash").Flash(1000000, null);
+}
+}}
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.isMouseButtonPressed(runtimeScene, "Left");
+if (isConditionTrue_0) {
+{gdjs.evtTools.sound.playSoundOnChannel(runtimeScene, "Music2.mp3", 1, true, 25, 1);
+}{runtimeScene.getGame().getVariables().getFromIndex(2).setNumber(1);
+}{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "Level 1", false);
+}}
+
+}
+
+
+};
+
+gdjs.Title_32ScreenCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Title_32ScreenCode.GDTitlescreenObjects1.length = 0;
+gdjs.Title_32ScreenCode.GDTitlescreenObjects2.length = 0;
+gdjs.Title_32ScreenCode.GDLogoObjects1.length = 0;
+gdjs.Title_32ScreenCode.GDLogoObjects2.length = 0;
+gdjs.Title_32ScreenCode.GDStartGameObjects1.length = 0;
+gdjs.Title_32ScreenCode.GDStartGameObjects2.length = 0;
+
+gdjs.Title_32ScreenCode.eventsList0(runtimeScene);
+gdjs.Title_32ScreenCode.GDTitlescreenObjects1.length = 0;
+gdjs.Title_32ScreenCode.GDTitlescreenObjects2.length = 0;
+gdjs.Title_32ScreenCode.GDLogoObjects1.length = 0;
+gdjs.Title_32ScreenCode.GDLogoObjects2.length = 0;
+gdjs.Title_32ScreenCode.GDStartGameObjects1.length = 0;
+gdjs.Title_32ScreenCode.GDStartGameObjects2.length = 0;
+
+
+return;
+
+}
+
+gdjs['Title_32ScreenCode'] = gdjs.Title_32ScreenCode;
---
+++ ourgame/code4.js
@@ -0,0 +1,75 @@
+gdjs.Level_324Code = {};
+gdjs.Level_324Code.localVariables = [];
+gdjs.Level_324Code.GDTheEndObjects1= [];
+gdjs.Level_324Code.GDTheEndObjects2= [];
+gdjs.Level_324Code.GDBackgroundObjects1= [];
+gdjs.Level_324Code.GDBackgroundObjects2= [];
+gdjs.Level_324Code.GDBlackBarsObjects1= [];
+gdjs.Level_324Code.GDBlackBarsObjects2= [];
+gdjs.Level_324Code.GDNestObjects1= [];
+gdjs.Level_324Code.GDNestObjects2= [];
+gdjs.Level_324Code.GDDuckArtworkObjects1= [];
+gdjs.Level_324Code.GDDuckArtworkObjects2= [];
+gdjs.Level_324Code.GDLadyDuckObjects1= [];
+gdjs.Level_324Code.GDLadyDuckObjects2= [];
+gdjs.Level_324Code.GDHeartObjects1= [];
+gdjs.Level_324Code.GDHeartObjects2= [];
+
+
+gdjs.Level_324Code.eventsList0 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+if (isConditionTrue_0) {
+{gdjs.evtTools.sound.playMusic(runtimeScene, "TheEnd.mp3", true, 100, 1);
+}}
+
+}
+
+
+};
+
+gdjs.Level_324Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Level_324Code.GDTheEndObjects1.length = 0;
+gdjs.Level_324Code.GDTheEndObjects2.length = 0;
+gdjs.Level_324Code.GDBackgroundObjects1.length = 0;
+gdjs.Level_324Code.GDBackgroundObjects2.length = 0;
+gdjs.Level_324Code.GDBlackBarsObjects1.length = 0;
+gdjs.Level_324Code.GDBlackBarsObjects2.length = 0;
+gdjs.Level_324Code.GDNestObjects1.length = 0;
+gdjs.Level_324Code.GDNestObjects2.length = 0;
+gdjs.Level_324Code.GDDuckArtworkObjects1.length = 0;
+gdjs.Level_324Code.GDDuckArtworkObjects2.length = 0;
+gdjs.Level_324Code.GDLadyDuckObjects1.length = 0;
+gdjs.Level_324Code.GDLadyDuckObjects2.length = 0;
+gdjs.Level_324Code.GDHeartObjects1.length = 0;
+gdjs.Level_324Code.GDHeartObjects2.length = 0;
+
+gdjs.Level_324Code.eventsList0(runtimeScene);
+gdjs.Level_324Code.GDTheEndObjects1.length = 0;
+gdjs.Level_324Code.GDTheEndObjects2.length = 0;
+gdjs.Level_324Code.GDBackgroundObjects1.length = 0;
+gdjs.Level_324Code.GDBackgroundObjects2.length = 0;
+gdjs.Level_324Code.GDBlackBarsObjects1.length = 0;
+gdjs.Level_324Code.GDBlackBarsObjects2.length = 0;
+gdjs.Level_324Code.GDNestObjects1.length = 0;
+gdjs.Level_324Code.GDNestObjects2.length = 0;
+gdjs.Level_324Code.GDDuckArtworkObjects1.length = 0;
+gdjs.Level_324Code.GDDuckArtworkObjects2.length = 0;
+gdjs.Level_324Code.GDLadyDuckObjects1.length = 0;
+gdjs.Level_324Code.GDLadyDuckObjects2.length = 0;
+gdjs.Level_324Code.GDHeartObjects1.length = 0;
+gdjs.Level_324Code.GDHeartObjects2.length = 0;
+
+
+return;
+
+}
+
+gdjs['Level_324Code'] = gdjs.Level_324Code;
